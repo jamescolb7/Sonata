6a6ce2cfa91bffe3c4ccc1c81f8d6e93035efbf0
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2024-03-12 23:35:16 -0400

    Player progress bar, additional controls

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,7 +3,7 @@
 import { useAtom } from "jotai";
 import { cn } from "@/lib/utils";
 import { Slider } from "./ui/slider";
-import { Pause, Play, SkipBack, SkipForward, Volume1 } from "lucide-react";
+import { Pause, Play, SkipBack, SkipForward, Volume, Volume1, Volume2 } from "lucide-react";
 import { PlayerAtom, QueueAtom, QueueIndexAtom } from "@/lib/PlayerState";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
@@ -30,6 +30,7 @@ export default function Player({
 	const [time, setTime] = useState<string | undefined>("00:00");
 	const [duration, setDuration] = useState<string | undefined>("00:00");
 	const [volume, setVolume] = useState<number>(100);
+	const [progress, setProgress] = useState<number>(0);
 
 	const playerRef = useRef<HTMLAudioElement>(null);
 
@@ -46,13 +47,23 @@ export default function Player({
 		setQueueIndex(queueIndex + 1);
 	}
 
+	const back = () => {
+		if (!queueIndex || !queue) return;
+		if (queueIndex <= 0) return;
+		setPlayer(queue[queueIndex - 1]);
+		setQueueIndex(queueIndex - 1);
+	}
+
 	useEffect(() => {
 		let playerElem: HTMLAudioElement | null = null;
 
 		if (playerRef.current) playerElem = playerRef.current;
 
 		const handleTime = () => {
-			setTime(formatTime(playerElem?.currentTime));
+			let time = playerElem?.currentTime as number || 0;
+			let duration = playerElem?.duration as number || 0;
+			setTime(formatTime(time));
+			setProgress(time / duration * 100)
 		}
 
 		const changeTrack = () => {
@@ -64,13 +75,38 @@ export default function Player({
 		playerElem?.addEventListener('play', () => setPaused(false));
 		playerElem?.addEventListener('pause', () => setPaused(true));
 
+		if (player?.album?.cover_medium) {
+			navigator.mediaSession.metadata = new MediaMetadata({
+				title: player?.title,
+				artist: player?.artist?.name,
+				album: player?.album?.title,
+				artwork: [
+					{
+						src: player?.album?.cover_medium,
+						sizes: '250x250'
+					}
+				]
+			})
+
+			navigator.mediaSession.setActionHandler('previoustrack', back);
+			navigator.mediaSession.setActionHandler('nexttrack', skip);
+			navigator.mediaSession.setActionHandler('play', () => {
+				setPaused(!paused);
+				playerRef?.current?.play();
+			});
+			navigator.mediaSession.setActionHandler('pause', () => {
+				setPaused(!paused);
+				playerRef?.current?.pause();
+			});
+		}
+
 		return () => {
 			playerElem?.removeEventListener('timeupdate', handleTime);
 			playerElem?.removeEventListener('loadedmetadata', changeTrack);
 			playerElem?.removeEventListener('play', () => setPaused(false));
 			playerElem?.removeEventListener('pause', () => setPaused(true));
 		}
-	}, [])
+	}, [player])
 
 	const togglePlay = () => {
 		setPaused(!paused);
@@ -87,8 +123,11 @@ export default function Player({
 
 	return (
 		<>
-			<div {...props} className={cn(className, "fixed w-full py-4 bottom-0 border-t bg-background align-center px-4")}>
-				<div className="flex flex-row flex-nowrap justify-between w-full">
+			<div {...props} className={cn(className, "fixed w-full bottom-0 border-t bg-background align-center ")}>
+				<div className='w-full h-[5px]'>
+					<div className={`bg-white rounded-sm h-full`} style={{ width: `${progress}%` }}></div>
+				</div>
+				<div className="flex flex-row flex-nowrap justify-between w-full py-3 px-4">
 					<div className="flex items-center space-x-3">
 						<Image height={56} width={56} className="h-14 w-14 rounded-md border indent-[-10000px]" src={player?.album ? player?.album?.cover_small : ""} alt="" />
 						<div>
@@ -105,12 +144,12 @@ export default function Player({
 						</div>
 					</div>
 					<div className="flex items-center flex-row space-x-3 cursor-pointer">
-						<SkipBack className="h-8 w-8" />
+						<SkipBack onClick={back} className="h-8 w-8" />
 						{paused ? <Play onClick={togglePlay} className="h-8 w-8" /> : <Pause onClick={togglePlay} className="h-8 w-8" />}
 						<SkipForward onClick={skip} className="h-8 w-8" />
 					</div>
 					<div className="flex items-center space-x-3 fixed invisible md:static md:visible">
-						<Volume1 className="h-8 w-8" />
+						{volume === 0 ? <Volume className="h-8 w-8" /> : volume < 50 ? <Volume1 className="h-8 w-8"></Volume1> : <Volume2 className="h-8 w-8"></Volume2>}
 						<Slider className="w-[100px]" onValueChange={changeVolume} defaultValue={[volume]} max={100} step={0.01}></Slider>
 						<p className="font-semibold">{time} / {duration}</p>
 					</div>
