acdcde95ec237bd72bc5385e078473d8b1fda3aa
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2024-04-08 13:18:34 -0400

    Signup and guest access security controls

--- server.ts
+++ server.ts
@@ -6,6 +6,7 @@ import { CSRF, Auth } from "./server/auth";
 
 const port = parseInt(process.env.PORT as string, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
+const guestsAllowed = process.env.GUESTS_ALLOWED as string ?? false;
 
 const app = next({ dev, hostname: '127.0.0.1', port });
 
@@ -21,8 +22,10 @@ app.prepare().then(() => {
 
 	server.all('*', (req: Request, res: Response) => {
 		const path = req.path ?? "";
-		//Allow some public routes, and next assets to be available without auth
-		if ((path.startsWith('/track/') || path.startsWith('/album/') || path.startsWith('/artist/') || path.startsWith('/_next/')) && !res.locals.user) return handle(req, res);
+		//Static assets
+		if (path.startsWith('/_next/')) return handle(req, res);
+		//Guest access
+		if ((path.startsWith('/track/') || path.startsWith('/album/') || path.startsWith('/artist/')) && !res.locals.user && guestsAllowed === "true") return handle(req, res);
 		if (!res.locals.user) return app.render(req, res, '/login');
 		//Only set the player url if the user is logged in
 		req.headers['player_url'] = process.env.PLAYER_URL as string
