c8f9efd7e369af528336539f1e8577ae29a1291d
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2024-09-07 22:56:33 -0400

    Update Node options

--- app/server/plugins/deezer.ts
+++ app/server/plugins/deezer.ts
@@ -7,12 +7,12 @@ export const pluginName = "deezer";
 const arl = process.env.ARL as string ?? null;
 
 export default async function DeezerDownload(id: string, quality: number) {
-    if (!arl) throw new Error('No ARL');
+    if (!arl) throw new Error('[Deezer Plugin] No ARL');
 
     const track = await dfi.getTrackInfo(id);
     const trackUrl = await dfi.getTrackDownloadUrl(track, quality);
 
-    if (!trackUrl) throw new Error('Failed to fetch track URL');
+    if (!trackUrl) throw new Error('[Deezer Plugin] Failed to fetch track URL');
 
     const res = await fetch(trackUrl.trackUrl).then(res => res.arrayBuffer());
     const buffer = Buffer.from(new Uint8Array(res));
@@ -22,13 +22,13 @@ export default async function DeezerDownload(id: string, quality: number) {
 }
 
 export async function init() {
-    if (!arl) return;
+    if (!arl) return console.log('[Deezer Plugin] Deezer ARL Not added as an env');
     await dfi.initDeezerApi(arl);
     try {
         if (!fs.existsSync(path)) fs.mkdirSync(path);
         if (!fs.existsSync(`${path}/${pluginName}`)) fs.mkdirSync(`${path}/${pluginName}`);
         await dfi.getUser();
-        console.log('Deezer Plugin Ready');
+        console.log('[Deezer Plugin] Plugin Ready');
     } catch (e: any) {
         console.error(e.message);
     }
